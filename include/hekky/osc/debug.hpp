90655e48286dd0401d5cd13ac7b77242554faeb0
Hyblocker <hyblocker@protonmail.com>
2022-11-26 15:56:38 +0100

    Add debug breaking

---
+++ include/hekky/osc/debug.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "hekky/osc/platform.hpp"
+
+#if defined(HEKKYOSC_WINDOWS)
+#define DEBUGBREAK() __debugbreak()
+#elif defined(HEKKYOSC_LINUX)
+#include <signal.h>
+#define DEBUGBREAK() raise(SIGTRAP)
+#else
+#pragma message(": warning HEKKYOSC: Debug break not available. Falling back to abort()...")
+#define DEBUGBREAK() abort()
+#endif

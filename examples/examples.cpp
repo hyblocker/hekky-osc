8f2f1951d10b4643b52dcfc787b28876196dc655
Hyblocker <hyblocker@protonmail.com>
2022-11-27 03:09:27 +0100

    Update examples.cpp

--- examples/examples.cpp
+++ examples/examples.cpp
@@ -4,69 +4,25 @@
 
 int main()
 {
-    srand(time(0));
-
     // Open a UDP socket, pointing to localhost on port 9000
     auto udpSender = hekky::osc::UdpSender("127.0.0.1", 9000);
 
-    auto message3 = hekky::osc::OscMessage("/test/one");
-    message3.Push(12);
-    udpSender.Send(message3);
-
-    auto message = hekky::osc::OscMessage("/tracking/trackers/1/position");
-    message.Push( (1000.f + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (1001.f - 1000.f)))) / 1000.f );
-    message.Push( (1000.f + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (1001.f - 1000.f)))) / 1000.f );
-    message.Push( (1000.f + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (1001.f - 1000.f)))) / 1000.f );
+    auto message = hekky::osc::OscMessage("/osc/test");
+    message.Push(12);
     udpSender.Send(message);
 
     auto message2 = hekky::osc::OscMessage("/osc/some/endpoint");
-    message2.Push("Wonderhoy!");
+    message2.Push("Hello World!");
     udpSender.Send(message2);
 
-    auto message4 = hekky::osc::OscMessage("/osc/some/endpoint");
-    message4.Push(5);
-    udpSender.Send(message4);
+    auto message3 = hekky::osc::OscMessage("/osc/vector/endpoint");
+    message3.Push(1.4142135624f); // sqrt(2)
+    message3.Push(3.1415926536f); // pi
+    message3.Push(2.7182818285f); // e
+    udpSender.Send(message3);
 
     // Closing it manually isn't needed, it gets closed via the destructor automatically!
     // udpSender.Close();
 
     std::cout << "Done!\n";
-
-    // Goal is to be able to do something similar with relative ease.
-    /*
-    
-    internal static class VrcOscHell
-    {
-        private static readonly string[] _endpoints = new string[]
-        {
-            "/tracking/trackers/1/position",
-            "/tracking/trackers/1/rotation",
-            "/tracking/trackers/2/position",
-            "/tracking/trackers/2/rotation",
-            "/tracking/trackers/3/position",
-            "/tracking/trackers/3/rotation",
-            "/tracking/trackers/head/position",
-            "/tracking/trackers/head/rotation"
-        };
-
-        internal static void Start()
-        {
-            var sender = new UDPSender("127.0.0.1", 9000);
-            var r = new Random();
-
-            while(true)
-            {
-                for (int i = 0; i < _endpoints.Length; i++)
-                {
-                    OscMessage packet = (i & 1) == 1
-                        ? new(_endpoints[i], r.Next(360), r.Next(360), r.Next(360))
-                        : new(_endpoints[i], r.Next(-1000, 1001) / 1000f, r.Next(-1000, 1001) / 1000f, r.Next(-1000, 1001) / 1000f);
-                    sender.Send(packet);
-                    Thread.Sleep(50);
-                }
-            }
-        }
-    }
-
-    */
 }
